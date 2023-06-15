ef7c8a4025cea4373a941d7f1e3d5e7e3679b948
wanderfulife <john.wander@hotmail.fr>
2023-06-15 17:56:48 +1000

    md

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,7 @@ export default function Home({ allPostsData }) {
 					(This is a sample website - you’ll be building a site like this on{" "}
 					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
 				</p>
+				Mathilde !! <Link href={`/posts/md`}>Clique ici</Link>
 			</section>
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px} `}>
 				<h2 className={utilStyles.headingLg}>Blog</h2>
@@ -36,9 +37,9 @@ export default function Home({ allPostsData }) {
 					{allPostsData.map(({ id, date, title }) => (
 						<li className={utilStyles.listItem} key={id}>
 							<Link href={`/posts/${id}`}>{title}</Link>
-							
+
 							<br />
-							<Date dateString={date}/>
+							<Date dateString={date} />
 						</li>
 					))}
 				</ul>
---
+++ pages/posts/md.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout, { siteTitle } from "../../components/layout";
+
+export default function Md() {
+	return (
+		<>
+			<Layout>
+				<Head>
+					<title>{siteTitle}</title>
+				</Head>
+				<h1>Je t'aime a la folie ❤️</h1>
+			</Layout>
+		</>
+	);
+}
