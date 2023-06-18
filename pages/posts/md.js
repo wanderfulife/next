b7b99cc9c7d4219a7e5eaed4e6a0b378fae62149
wanderfulife <john.wander@hotmail.fr>
2023-06-18 11:34:59 +1000

    Luv

--- pages/posts/md.js
+++ pages/posts/md.js
@@ -9,8 +9,7 @@ export default function Md() {
 				<Head>
 					<title>{siteTitle}</title>
 				</Head>
-				<h1>Je t'aime a la folie ❤️</h1>
-				<h1>Je t'aime a la folie  et plus encore</h1>
+				<h1>❤️</h1>
 			</Layout>
 		</>
 	);
