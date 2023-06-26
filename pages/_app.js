bd693cf2a53ba0b5e91e99dccdd2af232345d50e
wanderfulife <john.wander@hotmail.fr>
2023-06-27 06:30:57 +1000

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 // `pages/_app.js`
 import "../styles/global.css";
 
+export function reportWebVitals(metric) {
+	console.log(metric);
+}
+
 export default function App({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
